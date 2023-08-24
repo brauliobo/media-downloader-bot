372b501844bc0860154c76792bbbc1204c7d6b3e
Braulio Bhavamitra <brauliobo@gmail.com>
2023-08-23 22:11:04 -0300

    tagger: use relative path for picture

--- tagger.rb
+++ tagger.rb
@@ -2,11 +2,12 @@ require 'taglib'
 
 class Tagger
 
+  # FIXME: only works for audio, it removes other tags from video
   def self.add_cover fn, fthumb
-    # FIXME: only works for audio, it removes other tags from video
+    fthumb = Pathname.new(fthumb).relative_path_from Pathname.new(File.dirname fn)
     cmd = "kid3-cli"
     cmd << " -c \"cd #{Sh.escape File.dirname fn}\"  -c \"select #{Sh.escape File.basename fn}\""
-    cmd << " -c \"set picture:#{Sh.escape File.basename fthumb} COVER\""
+    cmd << " -c \"set picture:#{Sh.escape fthumb} COVER\""
     cmd << " -c save"
     Sh.run cmd
   end
