bee744142fd5a42480d57bc77eb1143fa814eafb
Braulio Bhavamitra <brauliobo@gmail.com>
2023-12-12 10:21:37 -0300

    processor: add aac type

--- bot/processor.rb
+++ bot/processor.rb
@@ -14,6 +14,7 @@ class Bot
     Rack::Mime::MIME_TYPES['.opus'] = 'audio/ogg'
     Rack::Mime::MIME_TYPES['.flac'] = 'audio/x-flac'
     Rack::Mime::MIME_TYPES['.caf']  = 'audio/x-caf'
+    Rack::Mime::MIME_TYPES['.aac']  = 'audio/x-aac'
     Rack::Mime::MIME_TYPES['.mkv']  = 'video/x-matroska'
 
     attr_reader :bot
