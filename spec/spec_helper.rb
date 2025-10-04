84b653723b68206e4bc17d6eced3e4932e1ea9ea
Braulio Oliveira <brauliobo@gmail.com>
2025-10-04 20:41:15 -0300

    Simplify spec_helper to require manager instead of individual files

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,1 @@
-require 'yaml'
-require 'active_support/core_ext/module/attribute_accessors'
-
-ENV['TRANSLATOR'] ||= 'Madlad400'
-ENV['LOCAL_GEMS'] ||= '1'
-
-require_relative '../lib/exts/sym_mash'
-require_relative '../lib/audiobook/parsers/pdf'
-require_relative '../lib/audiobook/book'
+require_relative '../lib/manager'
