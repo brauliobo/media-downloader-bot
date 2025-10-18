18d81700e9227b44395d7d8c623d22a391a399ac
Braulio Oliveira <brauliobo@gmail.com>
2025-10-18 13:05:27 -0300

    tts/coqui: increase default concurrency to 20

--- lib/tts/coqui_tts.rb
+++ lib/tts/coqui_tts.rb
@@ -7,7 +7,7 @@ class TTS
     configure_backend(
       base_url: "http://127.0.0.1:#{ENV['PORT']&.to_i || 10230}",
       segment_chars: 500,
-      concurrency: ENV['COQUITTS_CONCURRENCY']&.to_i || 10
+      concurrency: ENV['COQUITTS_CONCURRENCY']&.to_i || 20
     )
 
     extend self
