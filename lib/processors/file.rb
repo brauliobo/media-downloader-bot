0e489f6fdcf2d3af01ffb85cae54a927ac7539eb
Braulio Oliveira <brauliobo@gmail.com>
2025-11-03 22:20:03 -0300

    Extract common file download logic to File processor
    - Add download method to File processor for document/audio/video files
    - Add class_attribute :attr for media type configuration
    - Support both attr-based (audio/video) and document downloads

--- lib/processors/file.rb
+++ lib/processors/file.rb
@@ -1,19 +1,42 @@
 require_relative 'base'
+require 'active_support/core_ext/class/attribute'
 
 module Processors
   class File < Base
-    def process
-      pdoc = Processors::Document.new(dir:, bot:, msg:, st: self.st)
-      if pdoc.pdf_document? || pdoc.epub_document?
-        return pdoc.process
-      elsif msg.video.present?
-        return Processors::Video.new(dir:, bot:, msg:, st: self.st).process
-      elsif msg.audio.present?
-        return Processors::Audio.new(dir:, bot:, msg:, st: self.st).process
+    class_attribute :attr
+
+
+    def download
+      info = if attr
+        msg.send(attr)
       else
-        st.error('Unsupported message type')
+        msg.document
+      end
+
+      unless info
+        st.error("No #{attr || 'document'}")
         return
       end
+
+      local_path = if !attr && bot.respond_to?(:td_bot?) && bot.td_bot?
+        fid = info.respond_to?(:document) && info.document.respond_to?(:id) ? info.document.id : info.document.id
+        bot.download_file(fid, dir: dir)
+      else
+        bot.download_file(info, dir: dir)
+      end
+
+      file_opts = SymMash.new(self.opts.deep_dup.presence || {})
+      title = if info.respond_to?(:file_name)
+        info.file_name
+      else
+        ::File.basename(local_path, ::File.extname(local_path))
+      end
+
+      SymMash.new(
+        fn_in: local_path,
+        opts:  file_opts,
+        info:  { title: title },
+      )
     end
   end
 end
