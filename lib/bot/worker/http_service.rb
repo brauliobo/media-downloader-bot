56b69c68ed44aab0d6a781ba7f81dde6fbfad347
Braulio Oliveira <brauliobo@gmail.com>
2025-11-05 11:40:32 -0300

    Add Bot::Worker module with client, HTTP and DRb services

---
+++ lib/bot/worker/http_service.rb
@@ -0,0 +1,69 @@
+require 'roda'
+require 'json'
+
+module Bot
+  module Worker
+    class HTTPService < Roda
+      plugin :json
+      plugin :json_parser
+
+      def self.create(service)
+        Class.new(self) do
+          define_method :service do
+            service
+          end
+        end
+      end
+
+      def normalize_params(params)
+        params = params.dup
+        params.delete_if { |_, v| v.nil? }
+        params.symbolize_keys
+      end
+
+      route do |r|
+        r.post 'send_message' do
+          params = normalize_params(r.params)
+          msg = SymMash.new(params.delete(:msg))
+          text = params.delete(:text)
+          result = service.bot.send_message(msg, text, **params)
+          result.to_h
+        end
+
+        r.post 'edit_message' do
+          params = normalize_params(r.params)
+          msg = SymMash.new(params.delete(:msg))
+          id = params.delete(:id)
+          service.bot.edit_message(msg, id, **params)
+          {success: true}
+        end
+
+        r.post 'delete_message' do
+          params = normalize_params(r.params)
+          msg = SymMash.new(params.delete(:msg))
+          id = params.delete(:id)
+          service.bot.delete_message(msg, id, **params)
+          {success: true}
+        end
+
+        r.post 'download_file' do
+          params = normalize_params(r.params)
+          file_id_or_info = params.delete(:file_id_or_info)
+          result = service.bot.download_file(file_id_or_info, **params)
+          {path: result}
+        end
+
+        r.get 'queue/dequeue' do
+          timeout = r.params['timeout']&.to_f
+          job = service.dequeue(timeout: timeout)
+          job ? {job: job, service_uri: service.bot_service_uri} : {job: nil}
+        end
+
+        r.get 'queue/size' do
+          {size: service.queue_size}
+        end
+      end
+    end
+  end
+end
+
