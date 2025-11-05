6ecbe6c9dfe1b543fc1d7cb741142b308b7ad754
Braulio Oliveira <brauliobo@gmail.com>
2025-11-05 20:58:31 -0300

    Move common requires to boot.rb
    - Add active_support, json, faraday, rack/mime to boot.rb
    - Add sym_mash and peach requires to boot.rb
    - Add concurrent require to peach.rb

--- lib/boot.rb
+++ lib/boot.rb
@@ -8,3 +8,12 @@ Dir.chdir File.dirname(__FILE__) + '/..' do
   Dotenv.load! '.env'
 end
 
+require 'active_support/all'
+require 'json'
+require 'faraday'
+require 'faraday'
+require 'faraday/multipart'
+require 'rack/mime'
+
+require_relative 'exts/sym_mash'
+require_relative 'exts/peach'
