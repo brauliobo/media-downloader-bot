1322203c867393bbcc6c1d6f9264f6d211f5f206
Braulio Oliveira <brauliobo@gmail.com>
2025-11-06 21:49:21 -0300

    refactor: wrap HTTP client methods in class << self block
    Use class << self block for better organization of class methods
    in Utils::HTTP.

--- lib/utils/http.rb
+++ lib/utils/http.rb
@@ -3,15 +3,19 @@ require 'mechanize'
 module Utils
   class HTTP
 
-    def self.client
-      Thread.current[:utils_http] ||= Mechanize.new.tap do |a|
-        t = ENV['HTTP_TIMEOUT']&.to_i || 30.minutes
-        a.open_timeout = t
-        a.read_timeout = t
-      end
-    end
+    class << self
 
-    delegate_missing_to :client
+      def client
+        Thread.current[:utils_http] ||= Mechanize.new.tap do |a|
+          t = ENV['HTTP_TIMEOUT']&.to_i || 30.minutes
+          a.open_timeout = t
+          a.read_timeout = t
+        end
+      end
+
+      delegate_missing_to :client
+
+    end
 
   end
 end
