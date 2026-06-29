5603a05c2a7940103442d5baadf4f277317a3332
Braulio Oliveira <brauliobo@gmail.com>
2026-06-29 08:52:40 -0300

    input: keep bare url options

--- spec/processors/router_spec.rb
+++ spec/processors/router_spec.rb
@@ -12,4 +12,16 @@ RSpec.describe Processors::Router do
       expect(processors.first.ctx.url).to eq('https://x.com/i/status/2070518837150167314')
     end
   end
+
+  it 'keeps options on lines after a bare url' do
+    Dir.mktmpdir('router-spec-') do |dir|
+      ctx = Context.new(dir: dir, msg: SymMash.new)
+
+      processors = described_class.for_message(ctx, ['Cloooud |🇺🇦', 'x.com/i/status/2070518837150167314', 'audio'])
+
+      expect(processors.size).to eq(1)
+      expect(processors.first.ctx.url).to eq('https://x.com/i/status/2070518837150167314')
+      expect(processors.first.ctx.opts.audio).to eq(1)
+    end
+  end
 end
