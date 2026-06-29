3516fd4dcda002e712f31a981b62547789f8cfdc
Braulio Oliveira <brauliobo@gmail.com>
2026-06-29 08:41:49 -0300

    input: route bare social urls

--- lib/utils/input_parser.rb
+++ lib/utils/input_parser.rb
@@ -4,14 +4,16 @@ require 'uri'
 module Utils
   class InputParser
     Result = Data.define(:url, :opts)
+    URL_TOKEN_REGEXP = %r{\A(?:https?://)?(?:[a-z0-9-]+\.)+[a-z]{2,}(?::\d+)?(?:[/?#][^\s]*)?\z}i
 
     def self.parse(line)
       args = line.to_s.split(/[[:space:]]+/)
       url = nil
       
-      if (url_index = args.index { |arg| arg.match?(URI::DEFAULT_PARSER.make_regexp) })
+      if (url_index = args.index { |arg| url_like?(arg) })
         url_str = args[url_index]
         args    = args[(url_index + 1)..] || []
+        url_str = "https://#{url_str}" unless url_str.match?(%r{\Ahttps?://}i)
         url     = Addressable::URI.parse(url_str) rescue nil
       end
 
@@ -21,6 +23,10 @@ module Utils
       end
 
       Result.new(url: url, opts: opts)
+    end
+
+    def self.url_like?(token)
+      token.to_s.match?(URI::DEFAULT_PARSER.make_regexp) || token.to_s.match?(URL_TOKEN_REGEXP)
     end
   end
 end
--- spec/utils/input_parser_spec.rb
+++ spec/utils/input_parser_spec.rb
@@ -14,4 +14,11 @@ RSpec.describe Utils::InputParser do
     expect(parsed.url.to_s).to eq('https://example.com/v')
     expect(parsed.opts).to eq('speed' => '1.2')
   end
+
+  it 'normalizes bare urls before parsing' do
+    parsed = described_class.parse('x.com/i/status/2070518837150167314 audio')
+
+    expect(parsed.url.to_s).to eq('https://x.com/i/status/2070518837150167314')
+    expect(parsed.opts).to eq('audio' => 1)
+  end
 end
